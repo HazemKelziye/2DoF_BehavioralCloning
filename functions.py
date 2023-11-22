c53628c8f7637852b191471e1eb151b79803b4f0
vboxuser <vboxuser@Ubuntu3.myguest.virtualbox.org>
2023-11-22 13:56:42 +0300

    update figure's names

--- functions.py
+++ functions.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 def plot_response(data_dict):
     """plot the responses of the rocket while landing"""
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(8, 5))
     for key, value in data_dict.items():
         plt.plot(value, label=key)
     plt.legend()
     plt.grid()
     plt.ylim(-1.1, 1.1)
-    plt.title('2-DoF PID Control')
+    plt.title('2-DoF Behavioral-Cloning Control')
     plt.ylabel('Value')
     plt.xlabel('Steps')
     plt.show()
